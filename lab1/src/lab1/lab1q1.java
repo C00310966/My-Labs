4e428ff422907a2047debc2f0677135224916037
College <College@LAPTOP-0P1RUIEI.station>
2025-09-16 15:37:53 +0100

    Lab 1 Completed

---
+++ lab1/src/lab1/lab1q1.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class lab1q1 {
+
+	public static void main(String args[]) 
+	{ 
+		
+		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		thermA.setCelsius(20.0);
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+		double tempB;
+		Thermometer thermB = new Thermometer(10.0);
+		
+		tempB = thermB.getCelsius();
+		System.out.print("Temp. of Thermometer B is " + tempB);
+	}
+	
+}
